0084060e71cfaf1576ef7850d02b78b21b9fe831
mille-f <f11235tetra@gmail.com>
2017-05-02 02:20:17 +0900

    Lesson2 Chap2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :shops
+  resources :categories
   get 'welcome/index'
 
-  root 'welcome/index'
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
